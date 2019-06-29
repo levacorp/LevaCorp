e9aba8106c25d975d6b2b929bfcf77125edf375c
jcfv12330 <camilofv12330@gmail.com>
2019-06-29 08:55:49 -0500

    Agregada URL registro

--- Django-login/usuarios/urls.py
+++ Django-login/usuarios/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('homepage', views.homepage, name="homepage"),
     path('logout', views.logout_request, name='logout'),
     path('login', views.login_request, name="login"),
+    path('registrar', views.registrar, name="registrar"),
 ]
