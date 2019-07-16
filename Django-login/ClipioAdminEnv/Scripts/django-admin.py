65966fe33705203b055a49c859d789ea290f9751
daniel <danielfer@unicauca.edu.co>
2019-07-15 22:31:18 -0500

    Carga de informacion dinamica

---
+++ Django-login/ClipioAdminEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\semestreix\proyecto 1\respositorylevacorp\levacorp\django-login\clipioadmin\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
