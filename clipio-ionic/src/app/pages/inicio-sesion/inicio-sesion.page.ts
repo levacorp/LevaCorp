21fc71f772ee03171c2910474bb6ecfeac6cdb71
bdaniel <bdaniel@unicauca.edu.co>
2019-08-02 01:01:22 -0500

    agregado token inicio sesion

--- clipio-ionic/src/app/pages/inicio-sesion/inicio-sesion.page.ts
+++ clipio-ionic/src/app/pages/inicio-sesion/inicio-sesion.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+import { EmailValidator } from '@angular/forms';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioSesionPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('email') email;
+  @ViewChild('password') password;
+
+  constructor(private authServices: AuthenticationService, private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getDatosInicioSesion();
+  }
+
+  login() {
+    this.authServices.login();
+    console.log(this.email.value);
+    console.log(this.password.value);
   }
 
 }
