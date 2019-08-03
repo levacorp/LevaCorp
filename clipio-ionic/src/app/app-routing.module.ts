5ff8f2511cd6db7f0d10435e5abc617052aec380
bdaniel <bdaniel@unicauca.edu.co>
2019-08-02 22:07:27 -0500

    agregado servicio para encriptar

--- clipio-ionic/src/app/app-routing.module.ts
+++ clipio-ionic/src/app/app-routing.module.ts
@@ -5,33 +5,52 @@ import { AuthGuardService } from './services/auth-guard.service';
 const routes: Routes = [
 
   { path: '', redirectTo: 'inicio-sesion', pathMatch: 'full' },
-  { path: 'principal/:edificio', loadChildren: './pages/principal/principal.module#PrincipalPageModule', canActivate: [AuthGuardService]},
-  { path: 'preferencias', loadChildren: './pages/preferencias/preferencias.module#PreferenciasPageModule',
-   canActivate: [AuthGuardService] },
+  { path: 'principal/:edificio', loadChildren: './pages/principal/principal.module#PrincipalPageModule', canActivate: [AuthGuardService] },
+  {
+    path: 'preferencias', loadChildren: './pages/preferencias/preferencias.module#PreferenciasPageModule',
+    canActivate: [AuthGuardService]
+  },
   { path: 'edificio', loadChildren: './pages/edificio/edificio.module#EdificioPageModule', canActivate: [AuthGuardService] },
-  { path: 'crear-edificio', loadChildren: './pages/crear-edificio/crear-edificio.module#CrearEdificioPageModule',
-   canActivate: [AuthGuardService] },
-  { path: 'informacion-edificio/:argumento', loadChildren: './pages/informacion-edificio/informacion-edificio.module#InformacionEdificioPageModule', canActivate: [AuthGuardService] },
+  {
+    path: 'crear-edificio', loadChildren: './pages/crear-edificio/crear-edificio.module#CrearEdificioPageModule',
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'informacion-edificio/:argumento',
+    loadChildren: './pages/informacion-edificio/informacion-edificio.module#InformacionEdificioPageModule',
+    canActivate: [AuthGuardService]
+  },
   { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule', canActivate: [AuthGuardService] },
-  { path: 'dispositivos-elemento/:id',
-   loadChildren: './pages/dispositivos-elemento/dispositivos-elemento.module#DispositivosElementoPageModule' },
+  {
+    path: 'dispositivos-elemento/:id',
+    loadChildren: './pages/dispositivos-elemento/dispositivos-elemento.module#DispositivosElementoPageModule'
+  },
   { path: 'dispositivo/:id', loadChildren: './pages/dispositivo/dispositivo.module#DispositivoPageModule' },
   { path: 'crear-elemento/:elemento', loadChildren: './pages/crear-elemento/crear-elemento.module#CrearElementoPageModule' },
-  { path: 'registrar', loadChildren: './pages/registrar/registrar.module#RegistrarPageModule' },
-  { path: 'informacion-edificio',
-   loadChildren: './pages/informacion-edificio/informacion-edificio.module#InformacionEdificioPageModule', canActivate: [AuthGuardService] },
-  { path: 'asociacion-elemento-dispositivo',
-   loadChildren: './pages/asociacion-elemento-dispositivo/asociacion-elemento-dispositivo.module#AsociacionElementoDispositivoPageModule'},
-  { path: 'elementos-por-habitacion/:habitacion',
+  {
+    path: 'informacion-edificio',
+    loadChildren: './pages/informacion-edificio/informacion-edificio.module#InformacionEdificioPageModule', canActivate: [AuthGuardService]
+  },
+  {
+    path: 'asociacion-elemento-dispositivo',
+    loadChildren: './pages/asociacion-elemento-dispositivo/asociacion-elemento-dispositivo.module#AsociacionElementoDispositivoPageModule'
+  },
+  {
+    path: 'elementos-por-habitacion/:habitacion',
     loadChildren: './pages/elementos-por-habitacion/elementos-por-habitacion.module#ElementosPorHabitacionPageModule',
-     canActivate: [AuthGuardService]},
-  { path: 'dispositivos-elemento/:habitacion/:elemento',
-   loadChildren: './pages/dispositivos-elemento/dispositivos-elemento.module#DispositivosElementoPageModule' },
-  { path: 'crear-dispositivo/:habitacion/:dispositivo/:dir',
-   loadChildren: './pages/crear-dispositivo/crear-dispositivo.module#CrearDispositivoPageModule' },
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'dispositivos-elemento/:habitacion/:elemento',
+    loadChildren: './pages/dispositivos-elemento/dispositivos-elemento.module#DispositivosElementoPageModule'
+  },
+  {
+    path: 'crear-dispositivo/:habitacion/:dispositivo/:dir',
+    loadChildren: './pages/crear-dispositivo/crear-dispositivo.module#CrearDispositivoPageModule'
+  },
   { path: 'inicio-sesion', loadChildren: './pages/inicio-sesion/inicio-sesion.module#InicioSesionPageModule' },
-  { path: 'registro', loadChildren: './pages/registro/registro.module#RegistroPageModule' }
-
+  { path: 'registro', loadChildren: './pages/registro/registro.module#RegistroPageModule' },
+  { path: 'registrar', loadChildren: './pages/registrar/registrar.module#RegistrarPageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- clipio-ionic/src/app/pages/inicio-sesion/inicio-sesion.page.ts
+++ clipio-ionic/src/app/pages/inicio-sesion/inicio-sesion.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GenerateXMLService } from '../../services/generate-xml.service';
+import { Router } from '@angular/router';
+import { EncryptService } from '../../services/encrypt.service';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -9,21 +11,24 @@ import { GenerateXMLService } from '../../services/generate-xml.service';
   styleUrls: ['./inicio-sesion.page.scss'],
 })
 export class InicioSesionPage implements OnInit {
-  public inicioSesionForm: FormGroup;
 
   @ViewChild('email') email;
   @ViewChild('password') password;
 
   constructor(private authServices: AuthenticationService, private generateXMLService: GenerateXMLService,
-              public formBuilder: FormBuilder) {  }
+              public formBuilder: FormBuilder, private router: Router, private encryptService: EncryptService) {  }
 
   ngOnInit() {
   }
 
   login() {
-    alert('Bienvenido:' + this.email.value);
-    console.log('XML Inicio Sesion:', this.generateXMLService.crearXMLInicioSesion(this.email.value, this.password.value).toString());
+    this.password.value = this.encryptService.encrypt(this.password.value);
     this.authServices.login();
+    console.log('XML Inicio Sesion:', this.generateXMLService.crearXMLInicioSesion(this.email.value, this.password.value).toString());
+  }
+
+  registrar() {
+    this.router.navigate(['/registrar']);
   }
 
 }
